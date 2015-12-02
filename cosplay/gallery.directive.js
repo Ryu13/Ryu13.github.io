69fb0901d2c12202d1bad56c6b77fbadf8713d57
Ryu13 <yamada13@vt.edu>
2015-12-02 05:09:15 -0500

    attempting cosplay gallery

---
+++ cosplay/gallery.directive.js
@@ -0,0 +1,28 @@
+var app = angular.module('cosplay');
+
+app.directive('gallery', Gallery);
+
+Gallery.$inject = [];
+
+function Gallery() {
+    return {
+        restrict: 'E',
+        scope: {},
+        transclude: false,
+        templateUrl: 'cosplay/cosplay.template.html',
+        controller: GalleryController(),
+        link: function (scope, elem, attrs) {
+            var path = "C:/Users/RYU/Desktop/Rogerayamada.com/personal-rework/images/kirito/"
+            scope.pics = [];
+            for (var i = 0; i < 3; i++) {
+                scope.pics.push({src: path + "kirito" + (i+1)});  
+            }
+        }
+    }
+};
+
+
+
+function GalleryController() {
+    
+};
